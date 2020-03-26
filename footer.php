f92786f87190938022973b81fb44584affd2d30d
qwqdanchun <287182701@qq.com>
2020-03-26 22:33:35 +0800

    add bug

--- footer.php
+++ footer.php
@@ -1,5 +1,6 @@
 <footer>
     <p>Copyright © 2017-2020 <a href="//<?php echo $_SERVER['SERVER_NAME']; ?>"><?php echo bloginfo('name')?> </a>.
         </p>
-    <p><a class="egg" style="color: white!important" href="javascript:var%20s%20=%20document.createElement('script');s.type='text/javascript';document.body.appendChild(s);s.src='<?php echo get_stylesheet_directory_uri() . '/extend/asteroids.min.js' ?>';void(0);" title="彩蛋">彩蛋</a> </p>
+    <p><a class="egg" style="color: white!important" href="javascript:var%20s%20=%20document.createElement('script');s.type='text/javascript';document.body.appendChild(s);s.src='<?php echo get_stylesheet_directory_uri() . '/extend/asteroids.min.js' ?>';void(0);" title="小飞机">小飞机</a>
+    <a class="egg" style="color: white!important" href="javascript:var%20s%20=%20document.createElement('script');s.type='text/javascript';document.body.appendChild(s);s.src='<?php echo get_stylesheet_directory_uri() . '/extend/bug.js' ?>';void(0);" title="bug">bug</a> </p>
 </footer>
