227bb5507b5bd438d778365d2e64614240776306
qwqdanchun <287182701@qq.com>
2020-03-29 22:59:20 +0800

    modify the header

--- header.php
+++ header.php
@@ -1,3 +1,7 @@
+<?php
+$cat = get_the_category();
+$catid = $cat[0]->cat_ID;
+?>
 <!doctype html>
 <head>
     <meta name="viewport"
@@ -15,3 +19,41 @@
     <?php wp_head(); ?>
 </head>
 <body>
+    <div class="layui-container" id="main">
+    <div class="blog-title"><?php obj_title_icon();
+        bloginfo('name'); ?>
+        <div class="post-title"> - <?php the_title(); ?></div>
+        <div class="close"><i class="layui-icon layui-icon-close"></i>
+        </div>
+    </div>
+    <div class="toolbar">
+        <div class="layui-row">
+            <div class="obj_menu_header">
+                <?php get_nav_menu_obj(); ?>
+            </div>
+        </div>
+        <div class="layui-row">
+            <div class="layui-col-md1 <?php get_self_adaption_css()?>">
+                <div class="toobar-col"><i class="fa fa-arrow-left" aria-hidden="true"
+                                           onclick="javascript :history.back(-1)"></i>
+                    <i class="fa fa-arrow-right" aria-hidden="true" onclick="javascript :history.forward()"></i><i
+                            class="fa fa-arrow-up" aria-hidden="true"></i></div>
+            </div>
+            <div class="layui-col-md9 layui-col-xs-12 layui-col-sm-12">
+                <div class="toolbar-url"><img class="toobar-icon" src="<?php echo getImgDir('folder.ico') ?>"alt=""><span><a href="//<?php echo $_SERVER['SERVER_NAME']; ?>">本网站</a></span>>
+                                                <?php if (is_front_page()) {?>
+                                                <span>首页</span>
+                                                <?php }elseif(is_category()){?>
+                                                <span><a href="<?php echo get_category_link($catid)?>"><?php echo get_cat_name($catid)?></a></span>
+                                                <?php }elseif(is_page()){?>
+                                                <span><?php the_title() ?></span>
+                                                <?php }elseif(is_single()){?>
+                                                <span><a href="<?php echo get_category_link($catid) ?>"><?php echo get_cat_name($catid) ?></a></span>><span><?php the_title(); ?></span>
+                                                <?php }?>
+                </div>
+            </div>
+            <div class="layui-col-md2 <?php get_self_adaption_css()?>">
+                <?php get_search_obj(); ?>
+            </div>
+        </div>
+    </div>
--- page-home.php
+++ page-home.php
@@ -5,39 +5,6 @@
 get_header();
 the_post();
 ?>
-<div class="layui-container" id="main">
-    <div class="blog-title"><img class="toobar-icon" src="<?php echo getImgDir('folder.ico') ?>"
-                                 alt=""><?php /*bloginfo('name');*/
-        echo bloginfo('name') ?>
-        <div class="post-title"> - <?php the_title(); ?></div>
-        <div class="close"><i class="layui-icon layui-icon-close"></i>
-        </div>
-    </div>
-    <div class="toolbar">
-        <div class="layui-row">
-            <div class="obj_menu_header">
-                <?php get_nav_menu_obj(); ?>
-            </div>
-        </div>
-        <div class="layui-row">
-            <div class="layui-col-md1  <?php get_self_adaption_css()?>">
-                <div class="toobar-col"><i class="fa fa-arrow-left" aria-hidden="true"
-                                           onclick="javascript :history.back(-1)"></i>
-                    <i class="fa fa-arrow-right" aria-hidden="true" onclick="javascript :history.forward()"></i>
-                    <i class="fa fa-arrow-up" aria-hidden="true"></i></div>
-            </div>
-            <div class="layui-col-md9  layui-col-xs-12 layui-col-sm-12">
-                <div class="toolbar-url"><img class="toobar-icon" src="<?php echo getImgDir('folder.ico') ?>"
-                                              alt=""><span>
-                        <a href="//<?php echo $_SERVER['SERVER_NAME']; ?>">本网站</a></span>
-                    ><span><?php the_title() ?></span>
-                </div>
-            </div>
-            <div class="layui-col-md2 <?php get_self_adaption_css()?>">
-                <?php get_search_obj(); ?>
-            </div>
-        </div>
-    </div>
     <div class="layui-row content">
         <div class="layui-col-md2 sidebar <?php get_self_adaption_css() ?>">
             <?php get_sidebar(); ?>
